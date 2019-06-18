e934b9ff6a377a2a8a6bd05e62ef3c3e73bda9b1
mochiry0 <ryota.m.0930@gmail.com>
2019-06-18 18:04:44 +0900

    fix config

--- fish/config.fish
+++ fish/config.fish
@@ -1,23 +1,26 @@
 
 # Path
 set -U fish_user_paths /usr/local/bin
-set -g -x PATH /usr/local/bin /usr/sbin /usr/local/sbin $PATH
-set -g -x PATH /usr/local/opt/coreutils/libexec/gnubin $PATH
-set -g -x PATH /usr/local/opt/findutils/libexec/gnubin $PATH
-set -g -x MANPATH /usr/local/share/man $MAINPATH
-set -g -x MANPATH /usr/local/opt/coreutils/libexec/gnuman $MANPATH
-set -g -x MANPATH /usr/local/opt/findutils/libexec/gnuman $MANPATH
+set -gx PATH /usr/local/bin /usr/sbin /usr/local/sbin $PATH
+set -gx PATH /usr/local/opt/coreutils/libexec/gnubin $PATH
+set -gx PATH /usr/local/opt/findutils/libexec/gnubin $PATH
+set -gx MANPATH /usr/local/share/man $MAINPATH
+set -gx MANPATH /usr/local/opt/coreutils/libexec/gnuman $MANPATH
+set -gx MANPATH /usr/local/opt/findutils/libexec/gnuman $MANPATH
 set -U fish_user_paths (echo $fish_user_paths | tr ' ' '\n' | sort -u)
-set -g -x PATH $HOME/.pyenv/shims $PATH
+
+# Python PATH
+set -gx PATH $HOME/.pyenv/shims $PATH
 eval (pyenv init - | source)
 
+# Ruby PATH
+set -gx PATH $HOME/.rbenv/shims $PATH
+eval (rbenv init - | source)
+
+# Node.js Path
+set -gx PATH $HOME/.nodebrew/current/bin $PATH
+
 # Aliases
-alias vi="vim"
-alias la="ls -alhF"
-alias ll="ls -lhF"
-alias du="du -h"
-alias df="df -h"
-alias su="su -l"
 alias tree="tree -ACN"
 alias startco='VBoxManage startvm "CentOS7" --type headless'
 alias stopco='VBoxManage controlvm "CentOS7" acpipowerbutton'
